886df2c741ff6c7b957e3400e439e4b1c768a097
Amber Westlund <amberwestlund@Ambers-MacBook-Pro.local>
2019-03-09 14:30:12 -0600

    we have a tab

---
+++ app/src/main/java/com/example/gymguide/HomeActivity.java
@@ -0,0 +1,29 @@
+package com.example.gymguide;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gymguide.R;
+
+
+public class HomeActivity extends Fragment{
+
+    public HomeActivity() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
