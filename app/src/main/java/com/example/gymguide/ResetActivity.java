047f51e988ed848f678edbf9fee389c629a2e049
meganquinlan <meganannequinlan@gmail.com>
2019-03-09 15:25:49 -0600

    Add ResetActivity functionality!

--- app/src/main/java/com/example/gymguide/ResetActivity.java
+++ app/src/main/java/com/example/gymguide/ResetActivity.java
@@ -1,14 +1,62 @@
 package com.example.gymguide;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetActivity extends AppCompatActivity {
+
+    private EditText usernameText;
+    private Button btnReset;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset);
+
+        usernameText = (EditText) findViewById(R.id.etEmail);
+        btnReset = (Button) findViewById(R.id.btnReset);
+
+
+        auth = FirebaseAuth.getInstance();
+
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = usernameText.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Enter your registered email.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ResetActivity.this, "We have sent instructions to your email to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ResetActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+            }
+        });
     }
     
 }
