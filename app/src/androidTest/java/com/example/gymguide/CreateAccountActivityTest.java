8e875f4a85813afebc63967b4f0cada64752285b
Amber Westlund <amberwestlund@Ambers-MacBook-Pro.local>
2019-03-26 19:38:14 -0500

    test files added

---
+++ app/src/androidTest/java/com/example/gymguide/CreateAccountActivityTest.java
@@ -0,0 +1,16 @@
+package com.example.gymguide;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreateAccountActivityTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onStart() {
+    }
+}
