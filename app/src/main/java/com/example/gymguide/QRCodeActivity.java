3d522e45a3401ffaa7def6e9413314f7e52b1118
ntimmons1 <ntimmons@wisc.edu>
2019-04-05 13:05:10 -0500

    updates from master

--- app/src/main/java/com/example/gymguide/QRCodeActivity.java
+++ app/src/main/java/com/example/gymguide/QRCodeActivity.java
@@ -2,6 +2,7 @@ package com.example.gymguide;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,15 +10,25 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class QRCodeActivity extends AppCompatActivity {
     private Button scan_btn;
+    private String qr_result = "";
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode);
+
+        db = FirebaseFirestore.getInstance();
+
         scan_btn = (Button) findViewById(R.id.scan_btn);
         final Activity activity = this;
         scan_btn.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +44,7 @@ public class QRCodeActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -43,7 +55,38 @@ public class QRCodeActivity extends AppCompatActivity {
 
             }
             else{
+                qr_result = result.getContents().trim();
+
+//                Intent intent = new Intent(QRCodeActivity.this, SingleExerciseActivity.class);
+//                Bundle b = new Bundle();
+//                b.putString("key", qr_result);
+//                intent.putExtras(b);
+//                startActivity(intent);
+//                finish();
+                DocumentReference docRef = db.collection("exercise").document(qr_result);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        Exercise e = null;
+                        if (task.isSuccessful()){
+                            DocumentSnapshot document = task.getResult();
+                            if(document.exists()){
+                                e = document.toObject(Exercise.class);
+                                Intent gotoWorkoutActivityIntent = new Intent(QRCodeActivity.this, SingleExerciseActivity.class);
+                                gotoWorkoutActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                gotoWorkoutActivityIntent.putExtra("exercise", e);
+                                startActivity(gotoWorkoutActivityIntent );
+                            }else{
+                                Toast.makeText(QRCodeActivity.this,"No such document", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(QRCodeActivity.this,"get failed", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
                 Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+
             }
         }
         else {
