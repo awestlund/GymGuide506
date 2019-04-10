095799f6d587f8ab1aad1e8ae4f8371b26d7a58a
Amber Westlund <amberwestlund@Ambers-MacBook-Pro.local>
2019-04-10 16:12:51 -0500

    added user text input to QR code scanner

--- app/src/main/java/com/example/gymguide/QRCodeActivity.java
+++ app/src/main/java/com/example/gymguide/QRCodeActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,9 +21,12 @@ import com.google.zxing.integration.android.IntentResult;
 
 public class QRCodeActivity extends AppCompatActivity {
     private Button scan_btn;
+    private Button type_btn;
+    private TextView text_input;
     private String qr_result = "";
+    private String userInput = "";
     private FirebaseFirestore db;
-    boolean isExercise;
+    boolean isExercise = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +35,9 @@ public class QRCodeActivity extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
 
         scan_btn = (Button) findViewById(R.id.scan_btn);
+        type_btn = (Button) findViewById(R.id.type_btn);
+        text_input = (TextView) findViewById(R.id.typedQR);
+
         final Activity activity = this;
         scan_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +53,77 @@ public class QRCodeActivity extends AppCompatActivity {
             }
         });
 
+        type_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                userInput = text_input.getText().toString().trim();
+                onUserInput(userInput);
+            }
+        });
+
+    }
+
+    protected void onUserInput(String userInput){
+        if (userInput != "") {
+
+            qr_result = userInput;
+
+            try {
+                DocumentReference docRef = db.collection("exercise").document(qr_result);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        Exercise e = null;
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                e = document.toObject(Exercise.class);
+                                Intent gotoWorkoutActivityIntent = new Intent(QRCodeActivity.this, SingleExerciseActivity.class);
+                                gotoWorkoutActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                gotoWorkoutActivityIntent.putExtra("exercise", e);
+                                startActivity(gotoWorkoutActivityIntent);
+                            } else {
+                                isExercise = false;
+                            }
+                        } else {
+                            Toast.makeText(QRCodeActivity.this, "get failed", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                //TODO
+            }
+            try {
+                if (!isExercise) {
+                    DocumentReference dRef = db.collection("equipment").document(qr_result);
+                    dRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            Equipment e = null;
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    e = document.toObject(Equipment.class);
+                                    e.setEquipmentID(qr_result);
+                                    Intent gotoWorkoutActivityIntent = new Intent(QRCodeActivity.this, EquipmentActivity.class);
+                                    gotoWorkoutActivityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    gotoWorkoutActivityIntent.putExtra("equipment", e);
+                                    startActivity(gotoWorkoutActivityIntent);
+                                } else {
+                                    Toast.makeText(QRCodeActivity.this, "No such document", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(QRCodeActivity.this, "get failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                Toast.makeText(QRCodeActivity.this, "Incorrect Input", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -57,12 +135,6 @@ public class QRCodeActivity extends AppCompatActivity {
             } else {
                     qr_result = result.getContents().trim();
 
-//                Intent intent = new Intent(QRCodeActivity.this, SingleExerciseActivity.class);
-//                Bundle b = new Bundle();
-//                b.putString("key", qr_result);
-//                intent.putExtras(b);
-//                startActivity(intent);
-//                finish();
                     try {
                         DocumentReference docRef = db.collection("exercise").document(qr_result);
                         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
