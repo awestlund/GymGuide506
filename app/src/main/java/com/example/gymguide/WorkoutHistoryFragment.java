c504963eecec533d2658352921b088d571803a12
Amber Westlund <amberwestlund@Ambers-MacBook-Pro.local>
2019-04-28 18:10:35 -0500

    recycleview edits

--- app/src/main/java/com/example/gymguide/WorkoutHistoryFragment.java
+++ app/src/main/java/com/example/gymguide/WorkoutHistoryFragment.java
@@ -74,6 +74,7 @@ public class WorkoutHistoryFragment extends Fragment {
                 if(auth.getCurrentUser() != null) {
                     final RecyclerView compWorkoutsRV = (RecyclerView) rootView.findViewById(R.id.days_completed_workouts_recyclerview);
                     compWorkoutsRV.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+
                     final List<Exercise> wh = new ArrayList<>();
                     compWorkoutsRV.setAdapter(null);
                     db.collection("workoutHistory").document(auth.getUid()).collection("History").document(today).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
