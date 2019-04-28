1f8873165c3c362ec54d5d287ed1b0de15bc6fd8
Amber Westlund <amberwestlund@Ambers-MacBook-Pro.local>
2019-04-28 17:07:25 -0500

    merge

--- app/src/main/java/com/example/gymguide/HomeActivity.java
+++ app/src/main/java/com/example/gymguide/HomeActivity.java
@@ -1,11 +1,13 @@
 package com.example.gymguide;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
